57c9e466f27f56fa46e9af954b451922d1d0b902
David Wyde <david.wyde@gmail.com>
2013-12-20 22:51:23 -0600

    Add a simple multiplication exercise.

---
+++ test_exercises.py
@@ -0,0 +1,17 @@
+import unittest
+
+from exercises import SimpleMultiplication
+
+class TestNumberExercises(unittest.TestCase):
+	
+	def test_simple_multiplication(self):
+		s = SimpleMultiplication()
+		s.numbers = [2, 3, 5]
+		result = s.solve()
+		self.assertEqual(result, 30)
+		
+	
+	
+	
+if __name__ == '__main__':
+	unittest.main()
