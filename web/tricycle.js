40e6c5c8b07b7713bc56ab902a93bd8cbde0bcf8
David Wyde <david.wyde@gmail.com>
2013-12-21 21:27:33 -0600

    Use AngularJS.

--- web/tricycle.js
+++ web/tricycle.js
@@ -1,67 +1,16 @@
-var TRICYCLE = (function() {
+var tricycleApp = angular.module('tricycleApp', []);
+ 
+tricycleApp.controller('ExerciseMenuCtrl', function ($scope, $http) {
+	$http.get('exercises.json').success(function(data) {
+		$scope.exercises = data;
+		$scope.current = data[0];
+	});
 	
-	var allExercises = {};
-	
-	function Exercise(data) {
-		this.title = data.title;
-		this.problem = data.problem;
-		this.solutions = data.solutions;
-		this.hints = data.hints;
-		
-		allExercises[this.title] = this;
-	}
-	
-	Exercise.prototype.createElement = function() {
-		var container;
+	$scope.showExercise = function(exercise) {
+		$scope.current = exercise;
 	};
 	
-	function ExerciseGroup(groupObj) {
-		var exercise;
-		
-		this.label = groupObj.label;
-		this.exercises = [];
-		for (var i = 0; i < groupObj.exercises.length; i++) {
-			exercise = new Exercise(groupObj.exercises[i]);
-			this.exercises.push(exercise);
-		}
-	}
-	
-	ExerciseGroup.prototype.createElement = function() {
-		var container = document.createElement('div');
-		container.innerText = this.label;
-		container.className += ' exercise-group';
-		
-		var body = document.getElementsByTagName('body')[0];
-		body.appendChild(container);
-		
+	$scope.solve = function() {
+		console.log($scope.answer === $scope.current.solution);
 	};
-	
-	return {
-		makeExercises: function(data) {
-			var groupName,
-				group;
-			
-			for (groupName in data) {
-				if (data.hasOwnProperty(groupName)) {
-					var group = new ExerciseGroup(data[groupName]);
-					group.createElement();
-				}
-			}
-			
-		},
-		loadExercise: function(name) {
-		}
-	}
-}());
-
-(function() {
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4 && xhr.status == 200) {
-			var data = JSON.parse(xhr.responseText);
-			TRICYCLE.makeExercises(data);
-		}
-	};
-	xhr.open('GET', 'exercises.json', true);
-	xhr.send();
-}())
+});
