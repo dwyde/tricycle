70f945b2a7db41f91165dac5e214d48556d847ed
David Wyde <david.wyde@gmail.com>
2013-12-22 15:17:04 -0600

    Make a helper function to reset the exercise display.

--- tricycle.js
+++ tricycle.js
@@ -3,13 +3,16 @@ var tricycleApp = angular.module('tricycleApp', []);
 tricycleApp.controller('ExerciseMenuCtrl', function ($scope) {
 	
 	$scope.exercises = exercises;
-	$scope.current = exercises[0];
-	
-	$scope.showHint = false;
+	$scope.current = exercises[exercises.length - 1];
+	resetExercise();
 	
 	$scope.showExercise = function(exercise) {
 		$scope.current = exercise;
+		resetExercise();
+	};
+	
+	function resetExercise() {
 		$scope.answer = '';
 		$scope.showHint = false;
-	};
+	}
 });
