6a0c8ee5ac0444ce30e17b1eea347cdb4c628aec
David Wyde <david.wyde@gmail.com>
2013-12-21 22:31:36 -0600

    Add a helper test method.

--- test/test_math_exercises.py
+++ test/test_math_exercises.py
@@ -4,16 +4,18 @@ from tricycle.math_exercises import PiDigit, SimpleMultiplication
 
 class TestNumberExercises(unittest.TestCase):
 	
+	def check_answer(self, exercise, value):
+		result = exercise.to_json()
+		self.assertEqual(result['solution'], str(value))
+	
 	def test_simple_multiplication(self):
 		exercise = SimpleMultiplication()
 		exercise.numbers = [2, 3, 5]
-		result = exercise.to_json()
-		self.assertEqual(result['solution'], '30')
+		self.check_answer(exercise, 30)
 
 	def test_pi_digit(self):
 		exercise = PiDigit()
-		result = exercise.to_json()
-		self.assertEqual(result['solution'], '2')
+		self.check_answer(exercise, 2)
 
 if __name__ == '__main__':
 	unittest.main()
