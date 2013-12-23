bb558534f72e12e1c1bd1e9e17afb6608b742548
David Wyde <david.wyde@gmail.com>
2013-12-22 21:21:25 -0600

    Save and load correctly-answered questions from localStorage.

--- tricycle.js
+++ tricycle.js
@@ -4,6 +4,7 @@ tricycleApp.controller('ExerciseMenuCtrl', function ($scope) {
 	
 	$scope.exercises = EXERCISES;
 	$scope.current = EXERCISES[0];
+	readCorrect();
 	resetExercise();
 	
 	$scope.showExercise = function(exercise) {
@@ -15,9 +16,21 @@ tricycleApp.controller('ExerciseMenuCtrl', function ($scope) {
 		var result = $scope.current.solve(answer);
 		if (result) {
 			$scope.current.correct = true;
+			STORAGE.markCorrect($scope.current.title);
 		}
 		return result;
 	};
+	
+	function readCorrect() {
+		var exercise;
+		
+		for (var i = 0; i < $scope.exercises.length; i++) {
+			exercise = $scope.exercises[i];
+			if (STORAGE.isCorrect(exercise.title)) {
+				exercise.correct = true;
+			}
+		}
+	}
 	
 	function resetExercise() {
 		$scope.answer = '';
